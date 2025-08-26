a0f4675efb18206d293c645d649c111eb23a5b54
Michael Erdely <mike@erdelynet.com>
2025-08-26 14:39:57 -0400

    Default to oil showing hidden files

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -4,6 +4,9 @@ return {
   ---@type oil.SetupOpts
   opts = {
     skip_confirm_for_simple_edits = true,
+    view_options = {
+      show_hidden = true,
+    }
   },
   -- Optional dependencies
   dependencies = { { "echasnovski/mini.icons", opts = {} } },
