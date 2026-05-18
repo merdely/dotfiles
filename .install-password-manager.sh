bf6a082bbee594f14d46a5be2db234efda58ab6b
Michael Erdely <mike@erdelynet.com>
2026-05-18 13:55:53 -0400

    Install password manager on init

---
+++ .install-password-manager.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+[ -r /etc/os-release ] && source /etc/os-release
+
+unset sys
+case "$ID" in
+  arch|artix|cachyos|garuda|endeavouros|manjaro) sys=arch ;;
+esac
+case "$ID_LIKE"
+  *arch*) sys=arch ;;
+esac
+
+if [ "$sys" = "arch" ]; then
+  sudo pacman -Syu pass git
+fi
+
