63ef901241a782fa8dfe7d7577b538264bd8668e
Michael Erdely <mike@erdelynet.com>
2026-05-22 13:20:53 -0400

    more echos AND do not use sudo if not needed

--- .prep-chezmoi.sh
+++ .prep-chezmoi.sh
@@ -12,6 +12,7 @@ esac
 
 if [ "$sys" = "arch" ]; then
   if ! pacman -Q git gnupg pass > /dev/null 2>&1; then
+    echo "Installing git, gnupg, pass"
     sudo pacman -Syu --needed --noconfirm pass git gnupg
   fi
 fi
