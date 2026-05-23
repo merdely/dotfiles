d8d8a542f3f667e69ec2f03ab3a2a9516cb69d96
Michael Erdely <mike@erdelynet.com>
2026-05-23 17:43:03 -0400

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -710,93 +710,93 @@ vim.api.nvim_create_autocmd("VimEnter", {
         return ""
       end
 
-    -- File type with Nerd Font icon
-    local function file_type()
-      local ft = vim.bo.filetype
-      local icons = {
-        lua = "\u{e620} ", -- nf-dev-lua
-        python = "\u{e73c} ", -- nf-dev-python
-        javascript = "\u{e74e} ", -- nf-dev-javascript
-        typescript = "\u{e628} ", -- nf-dev-typescript
-        javascriptreact = "\u{e7ba} ",
-        typescriptreact = "\u{e7ba} ",
-        html = "\u{e736} ", -- nf-dev-html5
-        css = "\u{e749} ", -- nf-dev-css3
-        scss = "\u{e749} ",
-        json = "\u{e60b} ", -- nf-dev-json
-        markdown = "\u{e73e} ", -- nf-dev-markdown
-        vim = "\u{e62b} ", -- nf-dev-vim
-        sh = "\u{f489} ", -- nf-oct-terminal
-        bash = "\u{f489} ",
-        zsh = "\u{f489} ",
-        rust = "\u{e7a8} ", -- nf-dev-rust
-        go = "\u{e724} ", -- nf-dev-go
-        c = "\u{e61e} ", -- nf-dev-c
-        cpp = "\u{e61d} ", -- nf-dev-cplusplus
-        java = "\u{e738} ", -- nf-dev-java
-        php = "\u{e73d} ", -- nf-dev-php
-        ruby = "\u{e739} ", -- nf-dev-ruby
-        swift = "\u{e755} ", -- nf-dev-swift
-        kotlin = "\u{e634} ",
-        dart = "\u{e798} ",
-        elixir = "\u{e62d} ",
-        haskell = "\u{e777} ",
-        sql = "\u{e706} ",
-        yaml = "\u{f481} ",
-        toml = "\u{e615} ",
-        xml = "\u{f05c} ",
-        dockerfile = "\u{f308} ", -- nf-linux-docker
-        gitcommit = "\u{f418} ", -- nf-oct-git_commit
-        gitconfig = "\u{f1d3} ", -- nf-fa-git
-        vue = "\u{fd42} ", -- nf-md-vuejs
-        svelte = "\u{e697} ",
-        astro = "\u{e628} ",
-      }
+      -- File type with Nerd Font icon
+      local function file_type()
+        local ft = vim.bo.filetype
+        local icons = {
+          lua = "\u{e620} ", -- nf-dev-lua
+          python = "\u{e73c} ", -- nf-dev-python
+          javascript = "\u{e74e} ", -- nf-dev-javascript
+          typescript = "\u{e628} ", -- nf-dev-typescript
+          javascriptreact = "\u{e7ba} ",
+          typescriptreact = "\u{e7ba} ",
+          html = "\u{e736} ", -- nf-dev-html5
+          css = "\u{e749} ", -- nf-dev-css3
+          scss = "\u{e749} ",
+          json = "\u{e60b} ", -- nf-dev-json
+          markdown = "\u{e73e} ", -- nf-dev-markdown
+          vim = "\u{e62b} ", -- nf-dev-vim
+          sh = "\u{f489} ", -- nf-oct-terminal
+          bash = "\u{f489} ",
+          zsh = "\u{f489} ",
+          rust = "\u{e7a8} ", -- nf-dev-rust
+          go = "\u{e724} ", -- nf-dev-go
+          c = "\u{e61e} ", -- nf-dev-c
+          cpp = "\u{e61d} ", -- nf-dev-cplusplus
+          java = "\u{e738} ", -- nf-dev-java
+          php = "\u{e73d} ", -- nf-dev-php
+          ruby = "\u{e739} ", -- nf-dev-ruby
+          swift = "\u{e755} ", -- nf-dev-swift
+          kotlin = "\u{e634} ",
+          dart = "\u{e798} ",
+          elixir = "\u{e62d} ",
+          haskell = "\u{e777} ",
+          sql = "\u{e706} ",
+          yaml = "\u{f481} ",
+          toml = "\u{e615} ",
+          xml = "\u{f05c} ",
+          dockerfile = "\u{f308} ", -- nf-linux-docker
+          gitcommit = "\u{f418} ", -- nf-oct-git_commit
+          gitconfig = "\u{f1d3} ", -- nf-fa-git
+          vue = "\u{fd42} ", -- nf-md-vuejs
+          svelte = "\u{e697} ",
+          astro = "\u{e628} ",
+        }
 
-      if ft == "" then
-        return " \u{f15b} " -- nf-fa-file_o
+        if ft == "" then
+          return " \u{f15b} " -- nf-fa-file_o
+        end
+
+        return ((icons[ft] or " \u{f15b} ") .. ft)
       end
 
-      return ((icons[ft] or " \u{f15b} ") .. ft)
-    end
-
-    -- File size with Nerd Font icon
-    local function file_size()
-      local size = vim.fn.getfsize(vim.fn.expand("%"))
-      if size < 0 then
-        return ""
+      -- File size with Nerd Font icon
+      local function file_size()
+        local size = vim.fn.getfsize(vim.fn.expand("%"))
+        if size < 0 then
+          return ""
+        end
+        local size_str
+        if size < 1024 then
+          size_str = size .. "B"
+        elseif size < 1024 * 1024 then
+          size_str = string.format("%.1fK", size / 1024)
+        else
+          size_str = string.format("%.1fM", size / 1024 / 1024)
+        end
+        return " \u{f016} " .. size_str .. " " -- nf-fa-file_o
       end
-      local size_str
-      if size < 1024 then
-        size_str = size .. "B"
-      elseif size < 1024 * 1024 then
-        size_str = string.format("%.1fK", size / 1024)
-      else
-        size_str = string.format("%.1fM", size / 1024 / 1024)
-      end
-      return " \u{f016} " .. size_str .. " " -- nf-fa-file_o
-    end
 
-    -- Mode indicators with Nerd Font icons
-    local function mode_icon()
-      local mode = vim.fn.mode()
-      local modes = {
-        n       = "NORMAL",
-        i       = "INSERT",
-        v       = "VISUAL",
-        V       = "V-LINE",
-        ["\22"] = "V-BLOCK",
-        c       = "COMMAND",
-        s       = "SELECT",
-        S       = "S-LINE",
-        ["\19"] = "S-BLOCK",
-        R       = "REPLACE",
-        r       = "REPLACE",
-        ["!"]   = "SHELL",
-        t       = "TERMINAL",
-      }
-      return modes[mode] or (" \u{f059} " .. mode)
-    end
+      -- Mode indicators with Nerd Font icons
+      local function mode_icon()
+        local mode = vim.fn.mode()
+        local modes = {
+          n       = "NORMAL",
+          i       = "INSERT",
+          v       = "VISUAL",
+          V       = "V-LINE",
+          ["\22"] = "V-BLOCK",
+          c       = "COMMAND",
+          s       = "SELECT",
+          S       = "S-LINE",
+          ["\19"] = "S-BLOCK",
+          R       = "REPLACE",
+          r       = "REPLACE",
+          ["!"]   = "SHELL",
+          t       = "TERMINAL",
+        }
+        return modes[mode] or (" \u{f059} " .. mode)
+      end
 
       _G.mode_icon = mode_icon
       _G.git_branch = git_branch
