00b3f651671f82c2c4c5c57cb7bf58119f779e67
Michael Erdely <mike@erdelynet.com>
2026-05-23 19:54:53 -0400

    Update .config/nvim/lua/lazy_setup.lua

--- dot_config/nvim/lua/lazy_setup.lua
+++ dot_config/nvim/lua/lazy_setup.lua
@@ -9,3 +9,15 @@ require("lazy").setup({
     notify = false, -- get a notification when changes are found
   },
 })
+
+--- mini surround ---
+require("mini.surround").setup()
+-- Default Keymaps
+-- | `sa` | Add surrounding or Direct with 'saiw' |
+-- | `sd` | Delete surrounding |
+-- | `sr` | Replace surrounding |
+-- | `sf` | Find surrounding (right) |
+-- | `sF` | Find surrounding (left) |
+-- | `sh` | Highlight surrounding |
+-- | `sn` | Update n_lines |
+-- | `l` / `n` | as suffix for prev/next |
