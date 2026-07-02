7cefb7bb78db1eb7d8bba1225f2bdc31efd4e90e
Michael Erdely <mike@erdelynet.com>
2026-07-02 15:51:23 -0400

    Update .config/nvim/init.lua

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -158,8 +158,8 @@ vim.api.nvim_create_autocmd("FileType", {
 vim.lsp.config['lua_ls'] = {
   cmd = { 'lua-language-server' },
   filetypes = { 'lua' },
-  -- root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
-  root_markers = { '.luarc.json', '.luarc.jsonc' },
+  root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
+  -- root_markers = { '.luarc.json', '.luarc.jsonc' },
   settings = {
     Lua = {
       runtime = {
