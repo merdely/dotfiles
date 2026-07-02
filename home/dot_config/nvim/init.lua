c741bbec21a47ddaa2b36d026efa0d45737855e4
Michael Erdely <mike@erdelynet.com>
2026-07-02 09:27:44 -0400

    Update .config/nvim/init.lua

--- home/dot_config/nvim/init.lua
+++ home/dot_config/nvim/init.lua
@@ -1,31 +1,33 @@
-vim.opt.number = true                      -- Line numbers
-vim.opt.cursorline = true                  -- Highlight current line
-vim.opt.termguicolors = true               -- Enable 24-bit colors
-vim.cmd.colorscheme('lunaperche')          -- Colorscheme
-vim.opt.colorcolumn = "80"                 -- Column number where to show color column
-vim.opt.list = true                        -- Show list characters
+vim.opt.number = true                       -- Line numbers
+vim.opt.cursorline = true                   -- Highlight current line
+vim.opt.termguicolors = true                -- Enable 24-bit colors
+vim.cmd.colorscheme('lunaperche')           -- Colorscheme
+vim.opt.colorcolumn = "80"                  -- Column number where to show color column
+vim.opt.signcolumn = "yes"                  -- avoid text shifting when signs appear
+vim.opt.list = true                         -- Show list characters
 vim.opt.listchars = { tab = "» ", trail = "·", nbsp = "␣" }  -- Specify list chars
-vim.opt.showmatch = true                   -- Highlight matching brackets
-vim.opt.showmode = false                   -- Don't show mode in command line
-vim.opt.tabstop = 2                        -- Tab width
-vim.opt.shiftwidth = 2                     -- Indent width
-vim.opt.softtabstop = 2                    -- Soft tab stop
-vim.opt.expandtab = true                   -- Use spaces instead of tabs
-vim.opt.smartindent = true                 -- Smart auto-indenting
-vim.opt.breakindent = true                 -- Show wrapped lines indented
-vim.opt.ignorecase = true                  -- Case insensitive search
-vim.opt.smartcase = true                   -- Case sensitive if uppercase in search
-vim.opt.writebackup = false                -- Don't create backup before writing
-vim.opt.swapfile = false                   -- Don't create swap files
-vim.opt.undofile = true                    -- Persistent undo
-vim.opt.path:append("**")                  -- Include subdirectories in search
-vim.opt.mouse = "a"                        -- Enable mouse support
-vim.opt.selection = "inclusive"            -- Include last char in selection
+vim.opt.showmatch = true                    -- Highlight matching brackets
+vim.opt.showmode = false                    -- Don't show mode in command line
+vim.opt.tabstop = 2                         -- Tab width
+vim.opt.shiftwidth = 2                      -- Indent width
+vim.opt.softtabstop = 2                     -- Soft tab stop
+vim.opt.expandtab = true                    -- Use spaces instead of tabs
+-- vim.opt.smartindent = true                  -- Smart auto-indenting
+vim.opt.breakindent = true                  -- Show wrapped lines indented
+vim.opt.ignorecase = true                   -- Case insensitive search
+vim.opt.smartcase = true                    -- Case sensitive if uppercase in search
+vim.opt.writebackup = false                 -- Don't create backup before writing
+vim.opt.swapfile = false                    -- Don't create swap files
+vim.opt.undofile = true                     -- Persistent undo
+vim.opt.updatetime = 250                    -- faster CursorHold / diagnostics
+vim.opt.path:append("**")                   -- Include subdirectories in search
+vim.opt.mouse = "a"                         -- Enable mouse support
+vim.opt.selection = "inclusive"             -- Include last char in selection
 if vim.fn.has('clipboard') > 0 then
-  vim.opt.clipboard:append("unnamedplus")  -- Use system clipboard
+  vim.opt.clipboard:append("unnamedplus")   -- Use system clipboard
 end
-vim.opt.splitbelow = true                  -- Horizontal split creation position
-vim.opt.splitright = true                  -- Vertical split creation position
+vim.opt.splitbelow = true                   -- Horizontal split creation position
+vim.opt.splitright = true                   -- Vertical split creation position
 
 vim.keymap.set("v", "<", "<gv", { desc = "Indent left and reselect" })
 vim.keymap.set("v", ">", ">gv", { desc = "Indent right and reselect" })
