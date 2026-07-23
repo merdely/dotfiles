abcd6fb0ab7a0f78d2fa50ea88f82670e83fc11c
Michael Erdely <mike@erdelynet.com>
2026-07-23 06:58:54 -0400

    Add wezterm config

---
+++ dot_config/wezterm/wezterm.lua
@@ -0,0 +1,35 @@
+local wezterm = require 'wezterm'
+local config = wezterm.config_builder()
+local act = wezterm.action
+
+config.mouse_bindings = {
+  -- Right click sends "woot" to the terminal
+  {
+    event = { Down = { streak = 1, button = 'Right' } },
+    mods = 'NONE',
+    action = act.SendString 'woot',
+  },
+
+  -- Change the default click behavior so that it only selects
+  -- text and doesn't open hyperlinks
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'NONE',
+    action = act.CompleteSelection 'ClipboardAndPrimarySelection',
+  },
+
+  -- and make CTRL-Click open hyperlinks
+  {
+    event = { Up = { streak = 1, button = 'Left' } },
+    mods = 'CTRL',
+    action = act.OpenLinkAtMouseCursor,
+  },
+  -- NOTE that binding only the 'Up' event can give unexpected behaviors.
+  -- Read more below on the gotcha of binding an 'Up' event only.
+}
+
+config.font_size = 11
+config.font = wezterm.font 'DejaVuSansM Nerd Font Mono'
+config.color_scheme = 'deep'
+config.enable_tab_bar = false
+return config
