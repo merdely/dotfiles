7813bcdf4aa01c712c15e7b3fc336445767f8827
Michael Erdely <mike@erdelynet.com>
2026-07-23 16:34:56 -0400

    Add neptune

---
+++ dot_config/wezterm/mercury.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+function M.apply(config)
+  config.font_size = 16
+end
+
+return M
---
+++ dot_config/wezterm/neptune.lua
@@ -0,0 +1,7 @@
+local M = {}
+
+function M.apply(config)
+  config.font_size = 12
+end
+
+return M
