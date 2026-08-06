1a09fc99dee9274e40a82cbba9473f74d529f17b
Michael Erdely <mike@erdelynet.com>
2026-08-06 09:47:39 -0400

    Remove .config/nvim/lua/autocommands.lua Remove .config/nvim/lua/colorscheme.lua Remove .config/nvim/lua/diagnostics.lua Remove .config/nvim/lua/find.lua Remove .config/nvim/lua/formatting.lua Remove .config/nvim/lua/functions.lua Remove .config/nvim/lua/grep.lua Remove .config/nvim/lua/keymaps.lua Remove .config/nvim/lua/lsp.lua Remove .config/nvim/lua/netrw.lua Remove .config/nvim/lua/options.lua Remove .config/nvim/lua/plugins.lua Remove .config/nvim/lua/statusline.lua Remove .config/nvim/lua/treesitter.lua

--- dot_config/nvim/lua/colorscheme.lua
+++
@@ -1,7 +0,0 @@
-vim.api.nvim_create_autocmd("ColorScheme", {
-	callback = function()
-		vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	end,
-})
--- vim.cmd.colorscheme("catppuccin")
-vim.cmd.colorscheme("nightfly")
